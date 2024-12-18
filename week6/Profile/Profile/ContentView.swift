8dd4e08ba2c16ddc19da90f94b72b4f0404021a5
LeeZhengJie0516 <leezhengjie0516@e.neivce.edu.my>
2024-12-18 11:26:21 +0800

    Week7

--- week6/Profile/Profile/ContentView.swift
+++ week6/Profile/Profile/ContentView.swift
@@ -30,6 +30,7 @@ struct ContentView: View {
                 Text("Lee Zheng Jie")
                     .bold()
                     .font(.largeTitle)
+                    .foregroundColor(Color("fontcolor"))
                 
                 Text("SEDSC 2330244")
                     .bold()
--- week7/FavouriteArtist/FavouriteArtist/ContentView.swift
+++ week7/FavouriteArtist/FavouriteArtist/ContentView.swift
@@ -9,11 +9,103 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack {
+                HStack  {
+                    Spacer()
+                    Image(systemName: "bell.fill")
+                        .ignoresSafeArea()
+                    Image(systemName: "person.fill")
+                        .ignoresSafeArea()
+                }
+                Spacer()
+                VStack {
+                    Text("Favourite Artists")
+                        .font(.title)
+                        .bold()
+                        .padding(.leading, -140)
+                    
+                    HStack {
+                        Color.color1
+                            .cornerRadius(15)
+                            .overlay(
+                                HStack {
+                                    Text("Kenshi Yonezu")
+                                        .bold()
+                                        .font(.title2)
+                                        .padding(.top, 120)
+                                        .padding(.leading, 10)
+                                        .foregroundStyle(.fontcolor)
+                                    Image("yonezu")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 160, height: 160)
+                                }
+                            )
+                    }
+                    
+                    HStack {
+                        Color.color2
+                            .cornerRadius(15)
+                            .overlay(
+                                HStack {
+                                    Text("Chrissy Costanza")
+                                        .bold()
+                                        .font(.title2)
+                                        .multilineTextAlignment(.trailing)
+                                        .padding(.trailing, -40)
+                                        .padding(.top, 120)
+                                        .foregroundStyle(.fontcolor)
+                                    Image("chrissy")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 160, height: 160)
+                                }
+                            )
+                    }
+                    
+                    HStack {
+                        Color.color3
+                            .cornerRadius(15)
+                            .overlay(
+                                HStack {
+                                    Text("Hikaru Utada")
+                                        .bold()
+                                        .font(.title2)
+                                        .multilineTextAlignment(.trailing)
+                                        .padding(.trailing, 0)
+                                        .padding(.top, 120)
+                                        .foregroundStyle(.fontcolor)
+                                    Image("utada")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 160, height: 160)
+                                }
+                            )
+                    }
+                    
+                    HStack {
+                        Color.color4
+                            .cornerRadius(15)
+                            .overlay(
+                                HStack {
+                                    Text("Hozier")
+                                        .bold()
+                                        .font(.title2)
+                                        .multilineTextAlignment(.trailing)
+                                        .padding(.trailing, 70)
+                                        .padding(.top, 120)
+                                        .foregroundStyle(.fontcolor)
+                                    Image("hozier")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 160, height: 160)
+                                }
+                            )
+                    }
+                }
+                
+            }
         }
         .padding()
     }

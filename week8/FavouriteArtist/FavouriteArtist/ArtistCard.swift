12a1f06e11bbe4a678ad1b2464ff249993162583
LeeZhengJie0516 <leezhengjie0516@e.neivce.edu.my>
2025-01-08 11:03:42 +0800

    week8

---
+++ week8/FavouriteArtist/FavouriteArtist/ArtistCard.swift
@@ -0,0 +1,18 @@
+//
+//  ArtistCard.swift
+//  FavouriteArtist
+//
+//  Created by labuser on 08/01/2025.
+//
+
+import SwiftUI
+
+struct ArtistCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArtistCard()
+}

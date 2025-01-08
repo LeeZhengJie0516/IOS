5f6b5dcc2b05b3ef1bc30e3d32c245ad2d97c90e
LeeZhengJie0516 <leezhengjie0516@e.neivce.edu.my>
2025-01-08 11:57:15 +0800

    week8

---
+++ week8/Weather/Weather/Day.swift
@@ -0,0 +1,18 @@
+//
+//  Day.swift
+//  Weather
+//
+//  Created by labuser on 08/01/2025.
+//
+
+import SwiftUI
+
+struct Day: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Day()
+}

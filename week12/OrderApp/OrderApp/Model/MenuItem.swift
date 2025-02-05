252863edfbe69d26606c0de369ec08d33f17c523
LeeZhengJie0516 <leezhengjie0516@e.neivce.edu.my>
2025-02-05 12:13:01 +0800

    week13

---
+++ week12/OrderApp/OrderApp/Model/MenuItem.swift
@@ -0,0 +1,8 @@
+//
+//  MenuItem.swift
+//  OrderApp
+//
+//  Created by labuser on 05/02/2025.
+//
+
+import Foundation
---
+++ week12/OrderApp/OrderApp/View/OrderSummaryView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderSummaryView.swift
+//  OrderApp
+//
+//  Created by labuser on 05/02/2025.
+//
+
+import SwiftUI
+
+struct OrderSummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderSummaryView()
+}
